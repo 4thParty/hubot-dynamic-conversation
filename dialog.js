9cb6d23773c107f4b5baf9132afbfa1d6c78527a
Eugene Asiedu <ngene84@gmail.com>
2016-07-09 18:10:57 +0000

    Add docs in code
    Sanitize question before getting it to the user
    
    Change the `go` func of the Dialog class to start for the Conversation class to use
    
    Return dialog to the caller in case caller wants to user

--- dialog.js
+++ dialog.js
@@ -17,14 +17,26 @@ function Dialog(switchBoard, msg, messageOptions) {
   };
 
   this.data.source = msg.envelope.user;
-  this._go();
 }
 util.inherits(Dialog, EventEmitter);
 
 Dialog.prototype._invokeDialog = function (message, done) {
   var self = this;
+  var question = message.question.trim();
+  var code = question.charCodeAt(question.length - 1);
 
-  self.msg.reply(message.question);
+  if (!message.required) {
+    question = ((code === 46 || code === 63) ? question : question + '.') + ' Reply with `skip` to move to the next question.';
+  }
+
+  self.msg.reply(question);
+
+  if (!message.required) {
+    self.dialog.addChoice(/skip/i, function (dialogMessage) {
+      dialogMessage.reply('Ok. we are skipping this section!!');
+      done();
+    });
+  }
 
   if (message.answer.type === 'choice') {
     for (var j = 0, len = message.answer.options.length; j < len; j++) {
@@ -57,22 +69,29 @@ Dialog.prototype._invokeDialog = function (message, done) {
 
   // TODO: provide proper implementation of attachment
   if (message.answer.type === 'attachment') {
-    self.dialog.addChoice(/skip/i, function (dialogMessage) {
-      dialogMessage.reply('Ok. we are skipping this section!!');
-      done();
-    });
   }
 };
 
+/**
+ * Gets the data for the dialog
+ * @return {Object} The object that describes the dialog
+ */
 Dialog.prototype.fetch = function () {
   return this.data;
 };
 
 
-Dialog.prototype._go = function () {
+/**
+ * Starts the dialog with the user
+ * @return {null}
+ */
+Dialog.prototype.start = function () {
   var self = this;
   var cbs = [];
 
+  // set all the dialog messages as callback functions that will be run in series
+  // that means the next question won't be asked until the previous one has been
+  // dealt with
   for (var i = 0, l = self.messageOptions.length; i < l; i++) {
     (function (currIndex) {
       var message = self.messageOptions[currIndex];
@@ -87,6 +106,8 @@ Dialog.prototype._go = function () {
     done();
   });
 
+  // call the callbacks in series
+  // emit `end` when all is done or an error occurs
   series(cbs, function (err) {
     return self.emit('end', err);
   });
--- index.js
+++ index.js
@@ -1,27 +1,52 @@
 var Conversation = require('hubot-conversation');
 var Dialog = require('./dialog');
 
+/**
+ * The conversation mangager - It manages all the dialogs that happens with users
+ * and records it to Hubot's brain
+ * @param {Hubot} robot A hubot instance
+ */
 function DynamicConversation(robot) {
   this.robot = robot;
   this.switchBoard = new Conversation(robot, 'user');
   robot.brain.data.announcements = robot.brain.data.announcements || [];
 }
 
-DynamicConversation.prototype.constructor = DynamicConversation;
-
-DynamicConversation.prototype.start = function (msg, dynamicMsg, callback) {
-  var dialog = new Dialog(this.switchBoard, msg, dynamicMsg);
-  dialog.on('end', function (err, message) {
+/**
+ * Starts a new dialog
+ * @param  {Hubot.Response} - msg
+ * @param  {Array} - dialogOptions An array of dialog options. This array contains
+ * the messages with options that is used to communicate with the user
+ *   eg. dialogOptions = [
+ *     {
+ *       question: 'Do you have any trouble',
+ *       answer: {
+ *         type: 'text'
+ *       },
+ *       required: false,
+ *       error: 'Sorry, did not understand response'
+ *     }
+ *   ]
+ * @param  {Function} - callback   A function that is called when the dialog is done
+ * @return {Dialog} - A dialog instance
+ */
+DynamicConversation.prototype.start = function (msg, dialogOptions, callback) {
+  var dialog = new Dialog(this.switchBoard, msg, dialogOptions);
+  dialog.on('end', function (err) {
     if (err) {
-      return callback(err, message, dialog);
+      return callback(err, msg, dialog);
     }
 
     this.robot.brain.data.announcements.push(dialog.fetch());
-    return callback(null, message, dialog);
+    console.log(this.robot.brain.data.announcements);
+    return callback(null, msg, dialog);
   }.bind(this));
+  dialog.start();
+
+  return dialog;
 }
 
 module.exports = function (robot) {
-  var convo = new DynamicConversation(robot);
-  return convo;
+  var dynamicConversation = new DynamicConversation(robot);
+  return dynamicConversation;
 };
